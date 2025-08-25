c7478b394cdcbb6f9d56113166224b4ac1d877b0
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2025-08-25 16:05:16 -0300

    Criado component inicial de redefinir senha

---
+++ src/app/paginas/auth/redefinir-senha/redefinir-senha.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redefinir-senha',
+  imports: [],
+  templateUrl: './redefinir-senha.component.html',
+  styleUrl: './redefinir-senha.component.scss'
+})
+export class RedefinirSenhaComponent {
+
+}
