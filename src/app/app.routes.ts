187dbd52e893fe09f8dd69771c0d3212705d5763
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2025-09-03 18:58:53 -0300

    Adição inicial do dashboard após login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'auth/login' },
   { path: 'auth', loadChildren: () => import('./paginas/auth/auth.routes').then(m => m.AUTH_ROUTES) },
-  { path: '**', redirectTo: 'auth/login' }
+  { path: 'sistema', loadChildren: () => import('./paginas/sistema/sistema.routes').then(m => m.SISTEMA_ROUTES) },
+  { path: '**', redirectTo: 'auth/login' },
 ];
