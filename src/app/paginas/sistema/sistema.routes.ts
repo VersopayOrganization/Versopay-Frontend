030c45fcf5dbd880476bfebeb77a715d020dfa4e
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2025-09-09 23:26:27 -0300

    Criação do componente base do perfil

--- src/app/paginas/sistema/sistema.routes.ts
+++ src/app/paginas/sistema/sistema.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const SISTEMA_ROUTES: Routes = [
     { path: '', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
+    { path: 'perfil', loadComponent: () => import('./perfil/perfil.component').then(m => m.PerfilComponent) },
     { path: '**', redirectTo: '' }
 ];
