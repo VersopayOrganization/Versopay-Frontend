f3d40eed5b66b66e37dab37d18e2b5d2fa16ec45
Gabriel Forny <gabriel.muniz@quantumequity.com.br>
2025-09-16 01:58:01 -0300

    Parte inicial tela webhook

--- src/app/paginas/sistema/configuracoes/configuracoes.routes.ts
+++ src/app/paginas/sistema/configuracoes/configuracoes.routes.ts
@@ -6,8 +6,9 @@ export const CONFIGURACOES_ROUTES: Routes = [
         path: '',
         component: ShellComponent,
         children: [
-             { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+            { path: '', redirectTo: 'perfil', pathMatch: 'full' },
             { path: 'perfil', loadComponent: () => import('./perfil/perfil.component').then(m => m.PerfilComponent) },
+            { path: 'webhooks', loadComponent: () => import('./webhooks/webhooks.component').then(m => m.WebhooksComponent) },
             { path: '**', redirectTo: '' }
         ]
     }
